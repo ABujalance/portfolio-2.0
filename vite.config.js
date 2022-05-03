12e8f829bae032daf71f2e89fc8ebee60d06131e
Alberto Bujalance <abuj@ciklum.com>
2022-05-03 12:52:26 +0200

    updated deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/portfolio-2.0/",
   css: {
     preprocessorOptions: {
       scss: { additionalData: `@use "styles/colors";` },
