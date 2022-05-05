3487ad79b3c1266203e27a207421d97495083f90
ABujalance <abujalance@deloittedigital.es>
2022-05-05 21:47:28 +0200

    added basic nav

--- components.js
+++ components.js
@@ -1,3 +1,4 @@
 import { AppComponent } from "./components/app";
 import { LandingComponent } from "./components/landing/landing";
 import { AboutComponent } from "./components/about/about";
+import { NavComponent } from "./components/nav/nav";
---
+++ components/nav/nav.js
@@ -0,0 +1,28 @@
+import htmlTemplate from "./nav.html?raw";
+
+export class NavComponent extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.createElement("template");
+    template.innerHTML = htmlTemplate;
+    this.appendChild(template.content.cloneNode(true));
+  }
+
+  onScroll = () => {
+    debugger;
+    const navElement = document.getElementById("nav");
+    if (window.scrollY < window.innerHeight) {
+      navElement.classList.remove("show-nav");
+    } else {
+      navElement.classList.add("show-nav");
+    }
+  };
+
+  connectedCallback() {
+    debugger;
+    document.addEventListener("scroll", this.onScroll);
+  }
+}
+
+customElements.define("nav-component", NavComponent);
