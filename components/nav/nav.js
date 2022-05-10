989fdca74304d1db75b96daa699ab4ce52a1681c
ABujalance <abujalance@deloittedigital.es>
2022-05-10 19:46:36 +0200

    restore nav

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -8,6 +8,19 @@ export class NavComponent extends HTMLElement {
     template.innerHTML = htmlTemplate;
     this.appendChild(template.content.cloneNode(true));
   }
+
+  onScroll = () => {
+    const navElement = document.getElementById("nav");
+    if (window.scrollY < window.innerHeight) {
+      navElement.classList.remove("show-nav");
+    } else {
+      navElement.classList.add("show-nav");
+    }
+  };
+
+  connectedCallback() {
+    document.addEventListener("scroll", this.onScroll);
+  }
 }
 
 customElements.define("nav-component", NavComponent);
