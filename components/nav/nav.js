190d3a68442fb2b317958d8b097a13d9c9b2eaab
ABujalance <abujalance@deloittedigital.es>
2022-05-10 19:31:25 +0200

    remove old js scroll

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -8,21 +8,6 @@ export class NavComponent extends HTMLElement {
     template.innerHTML = htmlTemplate;
     this.appendChild(template.content.cloneNode(true));
   }
-
-  onScroll = () => {
-    debugger;
-    const navElement = document.getElementById("nav");
-    if (window.scrollY < window.innerHeight) {
-      navElement.classList.remove("show-nav");
-    } else {
-      navElement.classList.add("show-nav");
-    }
-  };
-
-  connectedCallback() {
-    debugger;
-    document.addEventListener("scroll", this.onScroll);
-  }
 }
 
 customElements.define("nav-component", NavComponent);
