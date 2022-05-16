a8d7c6b7abf41cddabe9ff3dbcdc7fb926cc2e86
ABujalance <abujalance@deloittedigital.es>
2022-05-16 19:31:42 +0200

    extra styles

--- components/skills/skill-set/skill-set.js
+++ components/skills/skill-set/skill-set.js
@@ -14,30 +14,40 @@ export class SkillSetComponent extends HTMLElementWithTemplate {
     super.connectedCallback();
     const { id } = this.attributes;
     this.buildSkillSet();
-    document.getElementById(`show-skills-${id.value}`).addEventListener("click", () => onShowSkillHandler(id.value));
-    document.getElementById(`close-${id.value}`).addEventListener("click", () => onShowSkillHandler(id.value));
+    document
+      .getElementById(`show-skills-${id.value}`)
+      .addEventListener("click", () => onShowSkillHandler(id.value));
+    document
+      .getElementById(`close-${id.value}`)
+      .addEventListener("click", () => onShowSkillHandler(id.value));
   }
 
   buildSkillSet() {
     const { id, skillList } = this.attributes;
     const skillListElement = this.querySelector(`#skill-set-${id.value}`);
+    skillListElement.ariaHidden = true;
     skillList.value.split(",").forEach((skill) => {
       const listElement = document.createElement("li");
       listElement.innerHTML = skill;
       skillListElement.appendChild(listElement);
     });
     const lastListElement = document.createElement("li");
-    lastListElement.innerHTML = `<button id="close-${id.value}" class="skill-set__list__close hide-desktop">close</button>`;
+    lastListElement.innerHTML = `<button id="close-${id.value}" tabindex="-1" class="skill-set__list__close hide-desktop">close</button>`;
     skillListElement.appendChild(lastListElement);
   }
 }
 
 function onShowSkillHandler(id) {
   const skillListElement = document.querySelector(`#skill-set-${id}`);
+  const skillCloseButton = document.querySelector(`#close-${id}`);
   if (skillListElement.classList.contains("show")) {
     skillListElement.classList.remove("show");
+    skillCloseButton.tabIndex = "-1";
+    skillListElement.ariaHidden = true;
   } else {
     skillListElement.classList.add("show");
+    skillCloseButton.tabIndex = null;
+    skillListElement.ariaHidden = false;
   }
 }
 
--- data/skills-data.js
+++ data/skills-data.js
@@ -3,10 +3,26 @@ export const skills = [
     icon: "web",
     id: "frontend",
     title: "Front-End",
-    skillList: ["skill but is a very longadhdagsdgds sentence lorem ipsum", "another skill shorter", "skill", "skill", "skill", "skill"],
+    skillList: [
+      "skill but is a very longadhdagsdgds sentence lorem ipsum",
+      "another skill shorter",
+      "skill",
+      "skill",
+      "skill",
+    ],
   },
-  { icon: "dns", id: "backend", title: "Back-End", skillList: ["skill", "skill", "skill", "skill"] },
-  { icon: "code", id: "code", title: "code", skillList: ["skill", "skill", "skill"] },
+  {
+    icon: "dns",
+    id: "backend",
+    title: "Back-End",
+    skillList: ["skill", "skill", "skill", "skill"],
+  },
+  {
+    icon: "code",
+    id: "code",
+    title: "code",
+    skillList: ["skill", "skill", "skill"],
+  },
 ];
 
 export const stats = [
