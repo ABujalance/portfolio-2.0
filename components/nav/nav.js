4f1178e3983f4d8f43075f89716209e945480a21
Alberto Bujalance <abuj@ciklum.com>
2022-05-18 16:53:30 +0200

    nav functionality

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -23,16 +23,51 @@ export class NavComponent extends HTMLElementWithTemplate {
     super.connectedCallback();
     document.addEventListener("scroll", this.onScroll);
     const navButton = document.getElementById("nav-button");
-    navButton.addEventListener("click", onShowNavHandler);
+    const navList = document.getElementById("nav-list");
+    const navClose = document.getElementById("nav-close");
+    navButton.addEventListener("click", (ev) => onShowNavHandler(ev, navList));
+    navClose.addEventListener("click", (ev) => onCloseNavHandler(ev, navList));
   }
 }
 
-function onShowNavHandler() {
-  const navList = document.querySelector("#nav-list");
+function onShowNavHandler(event, navList) {
+  event.stopPropagation();
+  event.preventDefault();
   if (navList.classList.contains("show")) {
-    navList.classList.remove("show");
+    closeNav(navList);
   } else {
-    navList.classList.add("show");
+    openNav(navList);
+  }
+}
+
+function onCloseNavHandler(event, navList) {
+  event.stopPropagation();
+  event.preventDefault();
+  closeNav(navList);
+}
+
+function closeNav(navList) {
+  navList.classList.remove("show");
+  document.body.classList.remove("modal");
+  document.removeEventListener("click", (e) => {
+    onClickOutsideNavHandler(e, navList);
+  });
+}
+
+function openNav(navList) {
+  navList.classList.add("show");
+  document.body.classList.add("modal");
+  document.addEventListener("click", (ev) => {
+    onClickOutsideNavHandler(ev, navList);
+  });
+}
+
+function onClickOutsideNavHandler(event, navList) {
+  event.stopPropagation();
+  event.preventDefault();
+  const isClickInsideElement = navList.contains(event.target);
+  if (!isClickInsideElement) {
+    closeNav(navList);
   }
 }
 
