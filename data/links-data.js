c1a66b83c91f794c9ca5d6a9565752907410b9ab
ABujalance <abujalance@deloittedigital.es>
2022-05-23 19:54:55 +0200

    added good looking footer

---
+++ data/links-data.js
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    url: "https://www.linkedin.com/in/alberto-bujalance-178062144/",
+    id: "linkedin",
+    title: "LinkedIn",
+    icon: "linkedin",
+  },
+  {
+    url: "https://github.com/ABujalance",
+    id: "github",
+    title: "Git Hub",
+    icon: "github",
+  },
+];
