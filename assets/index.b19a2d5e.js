6f9a74c016b8a8ae50fe55c32b2155ca4b53c03f
ABujalance <59972852+ABujalance@users.noreply.github.com>
2022-06-02 19:00:36 +0000

    Deploying to gh-pages from @ ABujalance/portfolio-2.0@a6f9f4ca7740de751694104c8319795e4fd4d15d 🚀

--- assets/index.b19a2d5e.js
+++ assets/index.b19a2d5e.js
@@ -8,6 +8,9 @@ var _=Object.defineProperty;var u=(n,e,t)=>e in n?_(n,e,{enumerable:!0,configura
 </section>
 <div class="app__anchor"></div>
 <section>
+  <projects-component></projects-component>
+</section>
+<section>
   <footer-component></footer-component>
 </section>
 <div id="footer"></div>
@@ -58,7 +61,7 @@ var _=Object.defineProperty;var u=(n,e,t)=>e in n?_(n,e,{enumerable:!0,configura
     </nav>
   </div>
 </div>
-`;class z extends a{constructor(){super();r(this,"onScroll",()=>{const t=document.getElementById("nav");window.scrollY<=window.innerHeight-200?t.classList.remove("show-nav"):t.classList.add("show-nav")});const t=document.createElement("template");t.innerHTML=S,this.appendChild(t.content.cloneNode(!0))}connectedCallback(){super.connectedCallback(),document.addEventListener("scroll",this.onScroll);const t=document.getElementById("nav-button"),s=document.getElementById("nav-list"),o=document.getElementById("nav-close"),l=document.getElementById("modal-background");t.addEventListener("click",i=>T(i,s)),o.addEventListener("click",i=>d(i,s)),l.addEventListener("click",i=>{d(i,s)})}}function T(n,e){n.stopPropagation(),n.preventDefault(),e.classList.contains("show")?m(e):x(e)}function d(n,e){n.stopPropagation(),n.preventDefault(),m(e)}function m(n){n.classList.remove("show"),document.body.classList.remove("modal")}function x(n){n.classList.add("show"),document.body.classList.add("modal")}customElements.define("nav-component",z);var H=`<div class="footer">
+`;class T extends a{constructor(){super();r(this,"onScroll",()=>{const t=document.getElementById("nav");window.scrollY<=window.innerHeight-200?t.classList.remove("show-nav"):t.classList.add("show-nav")});const t=document.createElement("template");t.innerHTML=S,this.appendChild(t.content.cloneNode(!0))}connectedCallback(){super.connectedCallback(),document.addEventListener("scroll",this.onScroll);const t=document.getElementById("nav-button"),s=document.getElementById("nav-list"),o=document.getElementById("nav-close"),l=document.getElementById("modal-background");t.addEventListener("click",i=>x(i,s)),o.addEventListener("click",i=>d(i,s)),l.addEventListener("click",i=>{d(i,s)})}}function x(n,e){n.stopPropagation(),n.preventDefault(),e.classList.contains("show")?m(e):z(e)}function d(n,e){n.stopPropagation(),n.preventDefault(),m(e)}function m(n){n.classList.remove("show"),document.body.classList.remove("modal")}function z(n){n.classList.add("show"),document.body.classList.add("modal")}customElements.define("nav-component",T);var H=`<div class="footer">
   <div class="footer__content"><contact-form-component /></div>
   <div class="footer__links"><footer-links-component /></div>
   <div class="font__size-standard font__color-base">This site was developed using vanilla-only tecnologies</div>
@@ -78,13 +81,13 @@ var _=Object.defineProperty;var u=(n,e,t)=>e in n?_(n,e,{enumerable:!0,configura
   </span>
   <ul id="skill-set-\${id}" class="skill-set__list font__size-m font__size__desktop-large-ml"></ul>
 </div>
-`;class A extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=$,this.appendChild(e.content.cloneNode(!0))}connectedCallback(){super.connectedCallback();const{id:e}=this.attributes;this.buildSkillSet(),document.getElementById(`show-skills-${e.value}`).addEventListener("click",()=>p(e.value)),document.getElementById(`close-${e.value}`).addEventListener("click",()=>p(e.value))}buildSkillSet(){const{id:e,skillList:t}=this.attributes,s=this.querySelector(`#skill-set-${e.value}`);s.ariaHidden=!0,t.value.split(",").forEach(l=>{const i=document.createElement("li");i.innerHTML=l,s.appendChild(i)});const o=document.createElement("li");o.innerHTML=`<button id="close-${e.value}" tabindex="-1" class="skill-set__list__close hide-desktop">close</button>`,s.appendChild(o)}}function p(n){const e=document.querySelector(`#skill-set-${n}`),t=document.querySelector(`#close-${n}`);e.classList.contains("show")?(e.classList.remove("show"),t.tabIndex="-1",e.ariaHidden=!0):(e.classList.add("show"),t.tabIndex=null,e.ariaHidden=!1)}customElements.define("skill-set-component",A);var N=`<div class="skill-bar">
+`;class j extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=$,this.appendChild(e.content.cloneNode(!0))}connectedCallback(){super.connectedCallback();const{id:e}=this.attributes;this.buildSkillSet(),document.getElementById(`show-skills-${e.value}`).addEventListener("click",()=>p(e.value)),document.getElementById(`close-${e.value}`).addEventListener("click",()=>p(e.value))}buildSkillSet(){const{id:e,skillList:t}=this.attributes,s=this.querySelector(`#skill-set-${e.value}`);s.ariaHidden=!0,t.value.split(",").forEach(l=>{const i=document.createElement("li");i.innerHTML=l,s.appendChild(i)});const o=document.createElement("li");o.innerHTML=`<button id="close-${e.value}" tabindex="-1" class="skill-set__list__close hide-desktop">close</button>`,s.appendChild(o)}}function p(n){const e=document.querySelector(`#skill-set-${n}`),t=document.querySelector(`#close-${n}`);e.classList.contains("show")?(e.classList.remove("show"),t.tabIndex="-1",e.ariaHidden=!0):(e.classList.add("show"),t.tabIndex=null,e.ariaHidden=!1)}customElements.define("skill-set-component",j);var N=`<div class="skill-bar">
   <span class="skill-bar__name">\${name}</span>
   <div class="skill-bar__progress-bar">
     <span id="\${id}">\${percentage}</span>
   </div>
 </div>
-`;class B extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=N,this.appendChild(e.content.cloneNode(!0))}connectedCallback(){super.connectedCallback();const{id:e,percentage:t}=this.attributes,s=this.querySelector(`#${e.value}`);s.style["max-width"]=t.value}}customElements.define("skill-bar-component",B);var I=`<form
+`;class A extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=N,this.appendChild(e.content.cloneNode(!0))}connectedCallback(){super.connectedCallback();const{id:e,percentage:t}=this.attributes,s=this.querySelector(`#${e.value}`);s.style["max-width"]=t.value}}customElements.define("skill-bar-component",A);var B=`<form
   id="contact-form"
   class="contact-form"
   action="https://formspree.io/f/xvollowv"
@@ -128,7 +131,7 @@ var _=Object.defineProperty;var u=(n,e,t)=>e in n?_(n,e,{enumerable:!0,configura
     apply.
   </p>
 </form>
-`;class j extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=I,this.appendChild(e.content.cloneNode(!0))}connectedCallback(){super.connectedCallback(),document.getElementById("contact-form").addEventListener("click",F)}}function F(){grecaptcha.execute("6LeL8A8gAAAAAGKCdX_tJbchHOvFlxiJYy6hJR9n",{action:"submit"}).then(n=>{document.getElementById("g-recaptcha-response").value=n})}customElements.define("contact-form-component",j);var q=`<div class="footer-links" id="footer-links"></div>
+`;class I extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=B,this.appendChild(e.content.cloneNode(!0))}connectedCallback(){super.connectedCallback(),document.getElementById("contact-form").addEventListener("click",F)}}function F(){grecaptcha.execute("6LeL8A8gAAAAAGKCdX_tJbchHOvFlxiJYy6hJR9n",{action:"submit"}).then(n=>{document.getElementById("g-recaptcha-response").value=n})}customElements.define("contact-form-component",I);var q=`<div class="footer-links" id="footer-links"></div>
 `;const P=[{url:"https://www.linkedin.com/in/alberto-bujalance-178062144/",id:"linkedin",title:"LinkedIn",icon:"linkedin"},{url:"https://github.com/ABujalance",id:"github",title:"Git Hub",icon:"github"}];var O='<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="96px" height="96px">    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"/></svg>',V='<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="60px" height="60px">    <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M10.496,8.403 c0.842,0,1.403,0.561,1.403,1.309c0,0.748-0.561,1.309-1.496,1.309C9.561,11.022,9,10.46,9,9.712C9,8.964,9.561,8.403,10.496,8.403z M12,20H9v-8h3V20z M22,20h-2.824v-4.372c0-1.209-0.753-1.488-1.035-1.488s-1.224,0.186-1.224,1.488c0,0.186,0,4.372,0,4.372H14v-8 h2.918v1.116C17.294,12.465,18.047,12,19.459,12C20.871,12,22,13.116,22,15.628V20z"/></svg>';const J={github:O,linkedin:V};class D extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=q,this.appendChild(e.content.cloneNode(!0))}buildFooterLinks(){const e=this.querySelector("#footer-links");P.forEach(t=>{const s=document.createElement("a");s.href=t.url,s.id=t.id,s.ariaLabel=t.title,s.innerHTML=J[t.icon],e.appendChild(s)})}connectedCallback(){this.buildFooterLinks(),super.connectedCallback()}}customElements.define("footer-links-component",D);var G=`<div class="about">
   <div class="about__content">
     <div>
@@ -140,4 +143,10 @@ var _=Object.defineProperty;var u=(n,e,t)=>e in n?_(n,e,{enumerable:!0,configura
     </p>
   </div>
 </div>
-`;class R extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=G,this.appendChild(e.content.cloneNode(!0))}}customElements.define("about-component",R);
+`;class R extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=G,this.appendChild(e.content.cloneNode(!0))}}customElements.define("about-component",R);var K=`<div class="projects">
+  <div class="projects__container">
+    <project-component></project-component>
+  </div>
+</div>
+`;class W extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=K,this.appendChild(e.content.cloneNode(!0))}}customElements.define("projects-component",W);var Y=`<!-- <div class="project">Hey</div> -->
+`;class X extends a{constructor(){super();const e=document.createElement("template");e.innerHTML=Y,this.appendChild(e.content.cloneNode(!0))}}customElements.define("project-component",X);
