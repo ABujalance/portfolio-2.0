9bfb921d54181eafbef3c3d388f282658ca216d0
Alberto Bujalance <abuj@ciklum.com>
2022-06-03 13:09:11 +0200

    improved performance with captcha

--- components/footer/contact-form/contact-form.js
+++ components/footer/contact-form/contact-form.js
@@ -13,14 +13,26 @@ export class ContactFormComponent extends HTMLElementWithTemplate {
   connectedCallback() {
     super.connectedCallback();
     const contactForm = document.getElementById("contact-form");
-    contactForm.addEventListener("click", onContactForm);
+    contactForm.addEventListener("focus", onFocusForm, true);
   }
 }
 
-function onContactForm() {
-  grecaptcha.execute("6LeL8A8gAAAAAGKCdX_tJbchHOvFlxiJYy6hJR9n", { action: "submit" }).then((token) => {
-    document.getElementById("g-recaptcha-response").value = token;
-  });
+function onFocusForm() {
+  const gachaScript = document.createElement("script");
+  gachaScript.src = "https://www.recaptcha.net/recaptcha/api.js?render=6LeL8A8gAAAAAGKCdX_tJbchHOvFlxiJYy6hJR9n";
+  document.head.appendChild(gachaScript);
+  document.getElementById("contact-form").removeEventListener("focus", onFocusForm, true);
+
+  var recaptchaInterval = setInterval(function () {
+    if (!window.grecaptcha || !window.grecaptcha.execute) {
+      return;
+    }
+
+    clearInterval(recaptchaInterval);
+    grecaptcha.execute("6LeL8A8gAAAAAGKCdX_tJbchHOvFlxiJYy6hJR9n", { action: "submit" }).then((token) => {
+      document.getElementById("g-recaptcha-response").value = token;
+    });
+  }, 1000);
 }
 
 customElements.define("contact-form-component", ContactFormComponent);
