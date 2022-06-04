f006860d5529fcfc1ea36eb9c33443286129b41c
ABujalance <abujalance@deloittedigital.es>
2022-06-04 15:53:44 +0200

    added projects

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,19 +1,56 @@
 export const projects = [
   {
+    id: "portfolio",
+    title: "Portfolio 2.0",
+    description:
+      "<span class='font__color-identity'><b>New portfolio</b></span> made using <span class='font__color-dark-accent'><b>vanilla-only</b></span> technologies. </br> It started as a way to test my knowledge in web development. </br>The design is simple but it's fast and reliable",
+    link: "https://tilemapper.abujalance.com/",
+    image:
+      "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654350516/portfolio_zdw0pb.png",
+    tags: [
+      "HTML",
+      "Css",
+      "Js",
+      "Web Components",
+      "Sass",
+      "Vite",
+      "Learning Experience",
+    ],
+  },
+  {
     id: "mapper",
     title: "Awesome Mapper",
-    description: "[POC] Map generator based on custom png files and a tile map",
+    description:
+      "<span class='font__color-identity'><b>Map generator</b></span> based on custom png files and a tile map.</br> It uses different size brushes and tools.",
     link: "https://tilemapper.abujalance.com/",
-    image: "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654191018/awesomeMapper_aaxyna.jpg",
-    tags: ["React", "Firebase"],
+    image:
+      "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654191018/awesomeMapper_aaxyna.jpg",
+    tags: [
+      "Work in Progress",
+      "React",
+      "Canvas",
+      "Firebase",
+      "Learning Experience",
+    ],
+  },
+  {
+    id: "old",
+    title: "portfolio 1.0",
+    description:
+      "This was my first portfolio. I made it when I <span class='font__color-identity'><b>started learning React</b></span>. The code is a bit messy and I used a template for the design. Is far less responsive and slower than this one but a good learning experience overall.",
+    link: "http://old.abujalance.com/",
+    image:
+      "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654347334/old_e0xjmc.png",
+    tags: ["React", "Sass", "Learning experience"],
   },
   {
     id: "caser",
     title: "Caser Insurance Agents Searcher",
     description:
-      "This project was part of the new web implementation. It allows an user to search nearby insurance agents and apply different filters and icons based on the type of agent",
+      "This project was part of the new Caser web implementation. </br> It allows an user to <span class='font__color-identity'><b>search nearby insurance agents</b></span> and apply different filters and icons based on the type of agent",
     link: "https://www.caser.es/conocenos/buscador-agentes",
-    image: "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654191015/caser_mapa_agentes_qbols4.jpg",
+    image:
+      "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654191015/caser_mapa_agentes_qbols4.jpg",
     tags: ["Java", "Liferay", "Javascript", "Maps"],
   },
   {
@@ -22,7 +59,8 @@ export const projects = [
     description:
       "This was part of my degree's final project. The assistant guides you through the configuration proccess of a tool. It was also integrated with a real robot which hears and answers the questions",
     link: "https://www.youtube.com/watch?v=zWELCudYRjY",
-    image: "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654191021/chatbot_nao_ryzeav.jpg",
+    image:
+      "https://res.cloudinary.com/abujalance/image/upload/w_400/v1654191021/chatbot_nao_ryzeav.jpg",
     tags: ["IBM Watson", "Python", "Node-Red"],
   },
 ];
